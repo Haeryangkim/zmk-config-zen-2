b75baaede198b098444ad1dd3d6b4d242f244f0a
Haeryangkim <lafalell32@me.com>
2024-07-26 14:28:08 +0900

    Update custom_status_screen.c

--- config/custom_status_screen.c
+++ config/custom_status_screen.c
@@ -14,7 +14,7 @@
 #include <zephyr/logging/log.h>
 LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
-LV_IMG_DECLARE(zenlogo);
+LV_IMG_DECLARE(bongocat);
  LV_IMG_DECLARE(layers2);
 
 #if IS_ENABLED(CONFIG_CUSTOM_WIDGET_BATTERY_STATUS)
@@ -70,7 +70,7 @@ lv_obj_t *zmk_display_status_screen() {
  #if !IS_ENABLED(CONFIG_ZMK_SPLIT_ROLE_CENTRAL)
   lv_obj_t *zenlogo_icon;
    zenlogo_icon = lv_img_create(screen);
-  lv_img_set_src(zenlogo_icon, &zenlogo);
+  lv_img_set_src(zenlogo_icon, &bongocat);
    lv_obj_align(zenlogo_icon, LV_ALIGN_BOTTOM_MID, 0, -5);
    #endif    
 
